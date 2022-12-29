1c2c7e1e103f9e5bb784cd1e1c07156450387c26
Milad <milad.4274@gmail.com>
2022-12-30 00:57:38 +0330

    exploring collate_fn

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-from torch.utils.data import dataloader
+from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
 from torchvision import transforms
 import os
@@ -15,10 +15,21 @@ def create_dataset(path,size_transforms, augment):
 
 
 def create_dataloader(dataset, batch_size):
-    data_loader = dataloader(
+    data_loader = DataLoader(
         dataset,
         batch_size,
         shuffle=True,
         num_workers=os.cpu_count()
     )
     return data_loader
+
+def dicom_collate_fn(batch):
+    """
+       batch: is a list of tuples with (example, label, length)
+             where 'example' is a tensor of arbitrary shape
+             and label/length are scalars
+    """
+    _, labels, lengths = zip(*batch)
+    print(labels)
+    print(lengths)
+    return None
